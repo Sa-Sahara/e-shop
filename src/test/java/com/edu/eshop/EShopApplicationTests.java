bb0a1254c0e70d5f774ee8c2421cb122d3d3d02c
Aleksandra Saharova <privet49069@gmail.com>
2023-08-05 18:51:04 +0600

    create maven project with mongodb

---
+++ src/test/java/com/edu/eshop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

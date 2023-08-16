0e58d436b8482e1d9ba1a3dc98d50ebda41cf123
Aleksandra Saharova <privet49069@gmail.com>
2023-08-16 23:45:25 +0600

    integration test of db connection

--- src/test/java/com/edu/eshop/EShopApplicationTests.java
+++ src/test/java/com/edu/eshop/EShopApplicationTests.java
@@ -1,13 +1,59 @@
 package com.edu.eshop;
 
+import com.edu.eshop.dto.ProductRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class EShopApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Container
+	static MongoDBContainer container = new MongoDBContainer("mongo"); //name of docker image
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", container::getReplicaSetUrl);
 	}
 
+	@Test
+	void shouldCreateProduct() throws Exception {
+		ProductRequest request = getProductRequest();
+		String requestString = objectMapper.writeValueAsString(request);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.contentType(requestString))
+				.andExpect(status().isCreated());
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("Google Pixel 9")
+				.description("nice")
+				.price(BigDecimal.TEN)
+				.build();
+	}
 }

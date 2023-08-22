9cac0850b7c9b0d1e0b6710553927f3c9de4498f
Aleksandra Saharova <privet49069@gmail.com>
2023-08-22 14:39:35 +0600

    Order mcrsrv model/serv/repo/cntr +mysqlDB connect

--- order/src/main/java/com/edu/order_mcrs/service/OrderService.java
+++ order/src/main/java/com/edu/order_mcrs/service/OrderService.java
@@ -1,26 +1,22 @@
 package com.edu.order_mcrs.service;
 
-import com.edu.order_mcrs.dto.InventoryResponse;
 import com.edu.order_mcrs.dto.OrderLineItemsDto;
 import com.edu.order_mcrs.dto.OrderRequest;
 import com.edu.order_mcrs.model.Order;
 import com.edu.order_mcrs.model.OrderLineItems;
 import com.edu.order_mcrs.repository.OrderRepository;
-import io.micrometer.observation.Observation;
-import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;

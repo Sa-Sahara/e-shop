36c3b473790f5680b20949d314e37dfbf09765b4
Aleksandra Saharova <privet49069@gmail.com>
2023-08-31 00:50:35 +0600

    BadRequestException

---
+++ order/src/main/java/com/edu/order_mcrs/exception/BadRequestException.java
@@ -0,0 +1,19 @@
+package com.edu.order_mcrs.exception;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+
+    private static final Logger logger = LogManager.getLogger(BadRequestException.class);
+
+    public BadRequestException() {
+    }
+    public BadRequestException(String message) {
+        super(message);
+        logger.error("BadRequestException: " + message);
+    }
+}

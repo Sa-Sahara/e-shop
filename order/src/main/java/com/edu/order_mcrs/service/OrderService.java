bbf82939edf8dd3fb043755d24aaf53d069b0996
Aleksandra Saharova <privet49069@gmail.com>
2023-10-02 21:57:40 +0600

    let multiple inventory services running

--- order/src/main/java/com/edu/order_mcrs/service/OrderService.java
+++ order/src/main/java/com/edu/order_mcrs/service/OrderService.java
@@ -24,7 +24,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -42,8 +42,8 @@ public class OrderService {
                 .toList();
 
         //call inventory service to check if product inStock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-microservice/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)

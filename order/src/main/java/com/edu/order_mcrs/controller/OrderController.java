ca6e114f41851217f5ad541520b967e023ed6b05
Aleksandra Saharova <privet49069@gmail.com>
2023-11-08 15:30:25 +0600

    implement Circuit Breaker

--- order/src/main/java/com/edu/order_mcrs/controller/OrderController.java
+++ order/src/main/java/com/edu/order_mcrs/controller/OrderController.java
@@ -2,12 +2,11 @@ package com.edu.order_mcrs.controller;
 
 import com.edu.order_mcrs.dto.OrderRequest;
 import com.edu.order_mcrs.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -15,10 +14,18 @@ import java.util.concurrent.CompletableFuture;
 public class OrderController {
 
     private final OrderService orderService;
+    private final String WARNING = "sth went wrong, please order once more time";
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
+    }
+
+    public String fallbackMethod(OrderRequest request, RuntimeException exception){
+        return WARNING;
     }
 }
